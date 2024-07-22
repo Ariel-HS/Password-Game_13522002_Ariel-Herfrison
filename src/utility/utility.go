119bf64ba9f8589d1495f383c740ae2fd8a6b3fe
Ariel-HS <arielherfrison@gmail.com>
2024-07-22 09:11:33 +0700

    feat: utility

---
+++ src/utility/utility.go
@@ -0,0 +1,24 @@
+package utility
+
+func IsLeap(year int) bool {
+	return (year%4 == 0 && year%100 != 0) || year%400 == 0
+}
+
+func CheckLeap(acc int, password []rune) bool {
+	if len(password) == 0 {
+		return false
+	}
+
+	c := password[0]
+
+	if c < '0' || c > '9' {
+		return false
+	}
+
+	n := int(c - '0')
+	if IsLeap(acc + n) {
+		return true
+	}
+
+	return CheckLeap(n*10, password[1:])
+}
